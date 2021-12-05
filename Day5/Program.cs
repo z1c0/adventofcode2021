ef9dbf8de90c4ef11f2a57140fa892bae0130251
z1c0 <wziegler@live.at>
2021-12-05 10:55:19 +0100

    Use Grid<T> helper

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using aoc;
 
 Console.WriteLine("Day 5 - START");
 var sw = Stopwatch.StartNew();
@@ -11,7 +12,7 @@ static void Part1(bool horizontalOnly)
 	var lines = ReadInput().ToList();
 	var maxX = Math.Max(lines.Max(l => l.From.X), lines.Max(l => l.To.X));
 	var maxY = Math.Max(lines.Max(l => l.From.Y), lines.Max(l => l.To.Y));
-	var grid = new int[maxY + 1, maxX + 1];
+	var grid = new Grid<int>(maxX + 1, maxY + 1);
 	foreach (var line in lines)
 	{
 		DrawLine(line, grid, horizontalOnly);
@@ -19,25 +20,14 @@ static void Part1(bool horizontalOnly)
 	Print(grid);
 }
 
-static void Print(int[,] grid)
+static void Print(Grid<int> grid)
 {
-	var h = grid.GetLength(0);
-	var w = grid.GetLength(0);
-	var count = 0;
-	for (var y = 0; y < h; y++)
-	{
-		for (var x = 0; x < w; x++)
-		{
-			if (grid[y, x] >= 2)
-			{
-				count++;
-			}
-		}
-	}
+	//grid.Print();
+	var count = grid.Count(x => x >= 2);
 	Console.WriteLine($"In {count} areas, at least 2 lines overlap.");
 }
 
-static void DrawLine(((int X, int Y) From, (int X, int Y) To) line, int[,] grid, bool horizontalOnly)
+static void DrawLine(((int X, int Y) From, (int X, int Y) To) line, Grid<int> grid, bool horizontalOnly)
 {
 	if (horizontalOnly && line.From.X != line.To.X && line.From.Y != line.To.Y)
 	{
@@ -63,12 +53,12 @@ static void DrawLine(((int X, int Y) From, (int X, int Y) To) line, int[,] grid,
 		dy = 1;
 	}
 
-	grid[line.From.Y, line.From.X]++;
+	grid[line.From.X, line.From.Y]++;
 	while (line.From.X != line.To.X || line.From.Y != line.To.Y)
 	{
 		line.From.X += dx;
 		line.From.Y += dy;
-		grid[line.From.Y, line.From.X]++;
+		grid[line.From.X, line.From.Y]++;
 	}
 }
 
