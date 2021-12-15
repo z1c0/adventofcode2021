f978e19dfd7c9844bb1a633aad563df5c8b0b4c8
z1c0 <wziegler@live.at>
2021-12-15 07:31:37 +0100

    Days 14 & 15

---
+++ Day14/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+Console.WriteLine("Day 14 - START");
+var sw = Stopwatch.StartNew();
+Part1(10);
+Part2(40);
+Console.WriteLine($"END (after {sw.Elapsed.TotalSeconds} seconds)");
+
+static void Part1(int steps)
+{
+	var (template, rules) = ReadInput();
+	var sb = new StringBuilder();
+	for (var i = 0; i < steps; i++)
+	{
+		sb.Append(template.First());
+		for (var j = 0; j < template.Length - 1; j ++)
+		{
+			var c = template[j];
+			var cc = template[j + 1];
+			var to = rules[(c, cc)];
+			sb.Append(to);
+			sb.Append(cc);
+		}
+		template = sb.ToString();
+		sb.Length = 0;
+	}
+	var dict = new Dictionary<char, int>();
+	foreach (var c in template)
+	{
+		if (!dict.TryAdd(c, 1))
+		{
+			dict[c]++;
+		}
+	}
+	var min = dict.Min(p => p.Value);
+	var minElement = dict.Single(p => p.Value == min);
+	var max = dict.Max(p => p.Value);
+	var maxElement = dict.Single(p => p.Value == max);
+	Console.WriteLine($"Most common ({maxElement}) - least common ({minElement}) = {max - min}.");
+}
+
+static void Part2(int steps)
+{
+	var (template, rules) = ReadInput();
+	var pairs = new Dictionary<(char c, char cc), long>();
+	for (var j = 0; j < template.Length - 1; j ++)
+	{
+		var c = template[j];
+		var cc = template[j + 1];
+		pairs[(c, cc)] = 1;
+	}
+
+	for (var i = 0; i < steps; i++)
+	{
+		var newPairs = new Dictionary<(char c, char cc), long>();
+		foreach (var k in pairs.Keys)
+		{
+			var n = pairs[k];
+			var to = rules[k];
+
+			var p1 = (k.c, to);
+			if (!newPairs.TryAdd(p1, n))
+			{
+				newPairs[p1] += n;
+			}
+			var p2 = (to, k.cc);
+			if (!newPairs.TryAdd(p2, n))
+			{
+				newPairs[p2] += n;
+			}
+		}
+		pairs = newPairs;
+	}
+
+	var dict = new Dictionary<char, long>
+	{
+		{ pairs.First().Key.c, pairs.First().Value }
+	};
+	foreach (var p in pairs)
+	{
+		var cc = p.Key.cc;
+		if (!dict.TryAdd(cc, p.Value))
+		{
+			dict[cc] += p.Value;
+		}
+	}
+	var min = dict.Min(p => p.Value);
+	var minElement = dict.Single(p => p.Value == min);
+	var max = dict.Max(p => p.Value);
+	var maxElement = dict.Single(p => p.Value == max);
+	Console.WriteLine($"Most common ({maxElement}) - least common ({minElement}) = {max - min}.");
+}
+
+static (string Template, Dictionary<(char, char), char> Rules) ReadInput()
+{
+	var template = string.Empty;
+	var rules = new Dictionary<(char, char), char>();
+	foreach (var line in File.ReadAllLines("input.txt"))
+	{
+		if (!string.IsNullOrEmpty(line))
+		{
+			var tokens = line.Split(" -> ");
+			if (tokens.Length == 1)
+			{
+				template = line;
+			}
+			else
+			{
+				rules.Add((tokens.First().First(), tokens.First().Last()), tokens.Last().Single());
+			}
+		}
+	}
+	return (template, rules);
+}
---
+++ Day15/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using aoc;
+
+Console.WriteLine("Day 15 - START");
+var sw = Stopwatch.StartNew();
+Part1(false);
+Part1(true);
+Console.WriteLine($"END (after {sw.Elapsed.TotalSeconds} seconds)");
+
+static void Part1(bool enlargeGrid)
+{
+	var grid = ReadInput();
+	if (enlargeGrid)
+	{
+		grid = EnlargeGrid(grid);
+	}
+	FindPath(grid);
+}
+
+static Grid<int> EnlargeGrid(Grid<int> grid)
+{
+	var largeGrid = new Grid<int>(grid.Width * 5, grid.Height * 5);
+	for (var y = 0; y < largeGrid.Height; y += grid.Height)
+	{
+		for (var x = 0; x < largeGrid.Width; x += grid.Width)
+		{
+			grid.ForEach((p) =>
+			{
+				var pp = (x + p.X, y + p.Y);
+				var risk = (grid[p] + x + y) % 9;
+				if (risk == 0) 
+				{
+					risk = 9;
+				}
+				largeGrid[pp] = risk;
+			});
+		}
+	}
+	return largeGrid;
+}
+
+static void FindPath(Grid<int> grid)
+{
+	var visitedNodes = new Dictionary<(int X, int Y), int>()
+	{
+		{ (0, 0), 0 }
+	};
+	var queue = new PriorityQueue<(int X, int Y), int>();
+	queue.Enqueue((0, 0), 0);
+	while (queue.Count > 0)
+	{
+		var p = queue.Dequeue();
+		var adjacentStates = grid.GetAdjacent4(p).ToList();
+		if (adjacentStates.Any())
+		{
+			foreach (var a in adjacentStates)
+			{
+				var risk = grid[a];
+				if (!visitedNodes.ContainsKey(a) || visitedNodes[a] > risk + visitedNodes[p])
+				{
+					visitedNodes[a] = risk + visitedNodes[p];
+					if (a.X == grid.Width - 1 && a.Y == grid.Height - 1)
+					{
+						Console.WriteLine($"Path found, total risk: {visitedNodes[a]}");
+					}
+					queue.Enqueue(a, risk);
+				}
+			}
+		}
+		else
+		{
+			System.Console.WriteLine("b");
+		}
+	}
+	//throw new InvalidOperationException();
+}
+
+static Grid<int> ReadInput()
+{
+	return IntGrid.FromFile("input.txt");
+}
