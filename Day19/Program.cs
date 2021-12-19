9574b2d9878facacf1c0b8da5b8f07b135f5edd9
z1c0 <wziegler@live.at>
2021-12-19 19:11:02 +0100

    Day19 (WIP)

---
+++ Day19/Program.cs
@@ -0,0 +1,154 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Day 19 - START");
+var sw = Stopwatch.StartNew();
+Part1();
+Console.WriteLine($"END (after {sw.Elapsed.TotalSeconds} seconds)");
+
+static void Part1()
+{
+	var scanners = ReadInput();
+	while (scanners.Count > 1)
+	{
+		for (var i = 0; i < scanners.Count; i++)
+		{
+			var found = false;
+			for (var j = i + 1; j < scanners.Count; j++)
+			{
+				var s1 = scanners[i];
+				var s2 = scanners[j];
+				Console.WriteLine($"matching ... ({i} and {j} of {scanners.Count})");
+				var m = Match(s1, s2);
+				if (m != null)
+				{
+					var c1 = m.Value.c1;
+					var c2 = m.Value.c2;
+					var p = m.Value.p;
+					Console.WriteLine("before: " + c1.Count);
+					foreach (var (x, y, z) in c2)
+					{ 
+						var pp = (p.X + x, p.Y + y, p.Z + z);
+						c1.Add(pp);
+					}
+					Console.WriteLine("before: " + c1.Count);
+					scanners.Remove(s1);
+					scanners.Remove(s2);
+					scanners.Add(c1);
+					found = true;
+					break;
+				}
+				if (found)
+				{
+					break;
+				}
+			}
+		}
+	}
+	Console.WriteLine(scanners.Single().Count);
+}
+
+static (HashSet<(int X, int Y, int Z)> c1, HashSet<(int X, int Y, int Z)> c2, (int X, int Y, int Z) p)? Match(HashSet<(int X, int Y, int Z)> scanner1, HashSet<(int X, int Y, int Z)> scanner2)
+{
+	var combinations1 = CreateCombinations(scanner1);
+	var combinations2 = CreateCombinations(scanner2);
+	foreach (var c1 in combinations1)
+	{
+		var matchesX = new HashSet<int>();
+		var matchesY = new HashSet<int>();
+		var matchesZ = new HashSet<int>();
+		foreach (var c2 in combinations2)
+		{
+			const int range = 2000;
+			for (var x = -range; x < range; x++)
+			{
+				var count = c2.Count(e2 => c1.Any(e1 => e1.X == e2.X + x));
+				if (count >= 12)
+				{
+					matchesX.Add(x);
+				}
+			}
+			for (var y = -range; y < range; y++)
+			{
+				var count = c2.Count(e2 => c1.Any(e1 => e1.Y == e2.Y + y));
+				if (count >= 12)
+				{
+					matchesY.Add(y);
+				}
+			}
+			for (var z = -range; z < range; z++)
+			{
+				var count = c2.Count(e2 => c1.Any(e1 => e1.Z == e2.Z + z));
+				if (count >= 12)
+				{
+					matchesZ.Add(z);
+				}
+			}
+		}
+		if (matchesX.Any() && matchesY.Any() && matchesZ.Any())
+		{
+			var x = matchesX.Single();
+			var y = matchesY.Single();
+			var z = matchesZ.Single();
+			foreach (var c2 in combinations2)
+			{
+				if (c2.Count(e => c1.Contains((e.X + x, e.Y + y, e.Z + z))) >= 12)
+				{
+					return (c1, c2, (x, y, z));
+				}
+			}
+		}
+	}
+	return null;
+}
+
+static IEnumerable<HashSet<(int X, int Y, int Z)>> CreateCombinations(HashSet<(int X, int Y, int Z)> scanner)
+{
+	static void Permutate(HashSet<(int X, int Y, int Z)>[] combinations, int i, int x, int y, int z)
+	{
+		combinations[i + 0].Add(( x,  y, -z));
+		combinations[i + 1].Add(( x,  y,  z));
+		combinations[i + 2].Add(( x, -y, -z));
+		combinations[i + 3].Add(( x, -y,  z));
+		combinations[i + 4].Add((-x,  y, -z));
+		combinations[i + 5].Add((-x,  y,  z));
+		combinations[i + 6].Add((-x, -y, -z));
+		combinations[i + 7].Add((-x, -y,  z));
+	}
+	var combinations = new HashSet<( int X, int Y, int Z)>[24];
+	for (var i = 0; i < combinations.Length; i++)
+	{
+		combinations[i] = new();
+	}
+	foreach (var (x, y, z) in scanner)
+	{
+		Permutate(combinations,  0, x, y, z);
+		Permutate(combinations,  8, y, x, z);
+		Permutate(combinations, 16, z, x, y);
+	}
+	return combinations;
+}
+
+static List<HashSet<(int X, int Y, int Z)>> ReadInput()
+{
+	var scanners = new List<HashSet<(int X, int Y, int Z)>>();
+	foreach (var line in File.ReadAllLines("input.txt"))
+	{
+		if (!string.IsNullOrEmpty(line))
+		{
+			if (line.StartsWith("---"))
+			{
+				scanners.Add(new ());
+			}
+			else
+			{
+				var tokens = line.Split(',');
+				scanners.Last().Add((
+					int.Parse(tokens[0]),
+					int.Parse(tokens[1]),
+					int.Parse(tokens[2])
+				));
+			}
+		}
+	}
+	return scanners;
+}
